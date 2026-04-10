90d0f1930be86abbb18e1b1e77999028eb31d369
Stu T <168220848+BA-B777W@users.noreply.github.com>
2026-04-10 04:49:46 +0000

    autograde

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // add javascript here
+let name1 = window.prompt("Enter First Name").trim();
+let first1 = name1.charAt(0).toUpperCase();
+let first2 = name1.slice(1).toLowerCase();
+let nameFinal = first1 + first2;
+let range;
 let guess = 0;
 let answer = 0;
 let guessCount = 0;
@@ -6,9 +11,10 @@ const scores = [];
 
 document.getElementById("playBtn").addEventListener("click", play);
 document.getElementById("guessBtn").addEventListener("click", makeGuess);
+document.getElementById("giveUpBtn").addEventListener("click", giveUp);
 
 function play(){
-    let range = 0;
+    range = 0;
     let levels = document.getElementsByName("level");
     for(let i=0; i<levels.length; i++){
         if(levels[i].checked){
@@ -23,26 +29,59 @@ function play(){
     guessBtn.disabled = false;
     giveUpBtn.disabled = false;
     playBtn.disabled = true;
+    startTimer();
 }
+
+let startTime;
+const times = [];
+function startTimer(){
+    startTime = Number(new Date().getTime());
+}
+function endTimer(){
+    let endTime = Number(new Date().getTime());
+    let elapsed = Number((endTime - startTime)/1000).toFixed(3);
+    let timeSum = 0;
+    times.push(elapsed);
+    for (let i=0;i<times.length;i++){
+        timeSum += Number(times[i]);
+    }
+    let average = Number(timeSum/times.length).toFixed(3);
+    avgTime.textContent = "Average Time: " + average;
+    times.sort(function(a,b){return a-b;});
+    fastest.textContent = "Fastest Game: " + times[0];
+}
+
+
 
 function makeGuess(){
     let guess = parseInt(document.getElementById("guess").value);
     if(isNaN(guess)){
-        msg.textContent = "Please enter a valid number";
+        msg.textContent = "Please enter a valid number " + nameFinal;
+        return;
     }
     guessCount++;
     if(guess == answer){
-        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        msg.textContent = "Correct! It took " + guessCount + " tries " + nameFinal;
         updateScore(guessCount);
         resetGame();
     }
     else if (guess < answer){
-        msg.textContent = "Too low, try again.";
+        msg.textContent = "Too low, try again " + nameFinal;
     }
     else{
-        msg.textContent = "Too high, try again.";
+        msg.textContent = "Too high, try again " + nameFinal;
+    }
+    if (Math.abs(guess - answer) <= 2){
+        rating.textContent = "Your guess: hot";
+    }
+    else if (Math.abs(guess - answer) <= 5){
+        rating.textContent = "Your guess: warm";
+    }
+    else {
+        rating.textContent = "Your guess: cold";
     }
 }
+ 
 function updateScore(score){
     scores.push(score);
     wins.textContent = "Total wins: " + scores.length;
@@ -61,12 +100,54 @@ function updateScore(score){
         }
     }
 }
+
 function resetGame(){
     msg.value = "";
     guessBtn.disabled = true;
     giveUpBtn.disabled = true;
     playBtn.disabled = false;
+    e.checked = true;
+    h.checked = false;
+    m.checked = false;
     e.disabled = false;
     h.disabled = false;
     m.disabled = false;
+    endTimer();
 }
+
+function giveUp(){
+    resetGame();
+    updateScore(range);
+    endTimer();
+}
+
+setInterval(() => {
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
+    
+    if (minutes < 10 && seconds < 10) {
+        let livetime = "Current time: " + hours + ":0" + minutes + ":0" + seconds;
+        time.textContent = livetime;
+    }
+    else if (minutes < 10){
+        let livetime = "Current time: " + hours + ":0" + minutes + ":" + seconds;
+        time.textContent = livetime;
+    }
+    else if (seconds < 10){
+        let livetime = "Current time: " + hours + ":" + minutes + ":0" + seconds;
+        time.textContent = livetime;
+    }
+    else {
+        let livetime = "Current time: " + hours + ":" + minutes + ":" + seconds;
+        time.textContent = livetime;
+    }
+}, 1000);
+
+let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let monthA = Number(new Date().getMonth());
+monthA = month[monthA];
+let day = ["1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
+let dayA = Number(new Date().getDate());
+dayA = day[dayA - 1];
+date.textContent = monthA + " " + dayA + ", " + new Date().getFullYear();
